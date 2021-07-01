d73ee422e9398a630d5c24313e365f64cac7b6fd
Francisco <franciscoparisalbero@gmail.com>
2021-07-01 12:49:50 +0200

    proyecto

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <TareaState>
         <Router>
           <Switch>
-            <Route exact path="/proyectos" component={Login} />
+            <Route exact path="/" component={Login} />
             <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-            <Route exact path="/" component={Proyectos} />
+            <Route exact path="/proyectos" component={Proyectos} />
           </Switch>
         </Router>
       </TareaState>
